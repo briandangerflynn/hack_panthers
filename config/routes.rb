135ad2deb00e6e230f9018b8b228d84a9b8a4bda
Yuna Kim <ykim632@gmail.com>
2016-05-20 13:42:43 -0400

    Add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   resources :owners
-  resources :renters
   resources :rooms
+  resources :renters
+
+  namespace :renters do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
   get '/signup' => 'renters#new'
   post '/signup' => 'renters#create'
+end
 end
