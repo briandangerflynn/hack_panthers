a6300587669b7811ac1be31d17af10232df43486
Brian Flynn <bdflynny@gmail.com>
2016-05-24 09:41:53 -0400

    add cleanups to nav, lander, and profiles

--- app/controllers/renters_controller.rb
+++ app/controllers/renters_controller.rb
@@ -20,10 +20,6 @@ class RentersController < ApplicationController
        redirect_to '/signup'
      end
   end
-  # def create
-  #   @renter = Renter.create(renter_params)
-  #   redirect_to @renter
-  # end
 
   def edit
     @renter = Renter.find(params[:id])
@@ -44,7 +40,7 @@ class RentersController < ApplicationController
 
   private
   def renter_params
-    params.require(:renter).permit(:name, :email, :password, :description, :has_pet)
+    params.require(:renter).permit(:name, :email, :password, :description, :has_pet, :video_url)
   end
 
 
