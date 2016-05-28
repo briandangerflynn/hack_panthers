0987f22e7d6b37bc25c159bbb036dfa74345c26e
Brian Flynn <bdflynny@gmail.com>
2016-05-28 13:29:37 -0400

    add favicon image

--- app/assets/javascripts/rooms_script.js
+++ app/assets/javascripts/rooms_script.js
@@ -19,9 +19,6 @@ $(document).ready(function() {
         data.forEach(function(room){
           $("#rooms-test").append("<div class='col s4'><div class='card small'><div class='card-image'><img class='activator' src='" + room['thumbnail_url'] + "'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4' id='roomcardtitle'>" + room['title'] + "</span><p id='roomcards'>" + room['price'] + "</p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'><a href='/rooms/" + room['id'] + "'>" + room['title'] + "</a></span><p id='roomcards'>" + room['price'] + "per month</p><p id='roomcards'>" + room['borough'] + "</p><p id='roomcards'>" + room['description'] + "</p><i class='card-title activator' id='closelink'>close</i></div></div></div>")
         })
-
-        // Loop over data (which is a collection of  Rooms)
-        // Build a little HTML box for each room and append to the DOM
       }
     });
   });
