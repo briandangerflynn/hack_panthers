65c4fc51c4d75162b255d8c97947b929ce1589b7
Yuna Kim <ykim632@gmail.com>
2016-09-17 17:46:39 -0400

    no changes made

--- app/assets/javascripts/rooms_script.js
+++ app/assets/javascripts/rooms_script.js
@@ -20,6 +20,9 @@ $(document).ready(function() {
           $("#rooms-test").append("<div class='col s4'><div class='card small'><div class='card-image'><img class='activator' src='" + room['thumbnail_url'] + "'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4' id='roomcardtitle'>" + room['title'] + "</span><p id='roomcards'>"$ + room['price'] + per month"</p><p id='roomcards'>"Posted: + room['created_at'].strftime(""%d %b" at "%H:%M") "</div><div class='card-reveal'><span class='card-title grey-text text-darken-4'><a href='/rooms/" + room['id'] + "'>" + room['title'] + "</a></span><p id='roomcards'>" + room['price'] + "per month</p><p id='roomcards'>" + room['borough'] + "</p><p id='roomcards'>" + room['description'] + "</p><i class='card-title activator' id='closelink'>close</i></div></div></div>")
         })
 
+
+
+
         // Loop over data (which is a collection of  Rooms)
         // Build a little HTML box for each room and append to the DOM
       }
